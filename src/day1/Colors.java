726157340df754a182cb6d7c08547beadcb7ef9c
Tamila <tamila.gadaeva@gmail.com>
2020-03-11 20:14:05 -0400

    added another color

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -2,7 +2,7 @@ package day1;
 
 public class Colors {
     public static void main(String[] args) {
-        String [] colors = {"red", "blue", "green"};
+        String [] colors = {"red", "blue", "green", "yellow"};
 
     }
 }
